a9ed2eebeda097eacae202bbb1914789600beb8e
pkbtc <parasarkashyap06@gmail.com>
2024-07-07 16:07:38 +0530

    adding search page header

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchHeader from '@/components/SearchHeader'
+import "./../globals.css";
+const layout = ({children}) => {
+  return (
+    <div>
+        <SearchHeader/>
+        {children}
+    </div>
+  )
+}
+
+export default layout
