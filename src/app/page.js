4d3b5517d4fafa99ce13bda94e550c1caeb6b8fa
pkbtc <parasarkashyap06@gmail.com>
2024-07-07 15:26:12 +0530

    adding body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import HomeHeader from '@/components/HomeHeader'
+import Image from 'next/image'
+import HomeSeacrch from '@/components/HomeSeacrch'
 const Page= () => {
   return (
     <>
     
     <HomeHeader/>
+    <div className='flex flex-col items-center m-24'>
+    <Image
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'
+          alt='Google Logo'
+          width={300}
+          height={100}
+          priority
+          style={{width:'auto',height:'auto'}}
+          
+        />
+        <HomeSeacrch/>
+    </div>
     
     </>
   )
---
+++ src/components/HomeSeacrch.jsx
@@ -0,0 +1,53 @@
+'use client'
+
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { FaMicrophone } from "react-icons/fa";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+const HomeSeacrch = () => {
+    const [input,setInput]=useState('');
+    const [randomSearchLoadig,setRandomSearchLoadig]=useState(false);
+    const router=useRouter();
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        if(!input.trim()){
+            return;
+        }
+        router.push(`/search/web/?searchTerm=${input}`);
+        
+    }
+    const randomSearch=async(e)=>{
+        setRandomSearchLoadig(true);
+            const respones=await fetch(`https://random-word-api.herokuapp.com/word`).then((res)=>res.json()).then((data)=>data[0]);
+            if(!respones){
+                return
+            }
+            router.push(`/search/web/?searchTerm=${respones}`);
+            setRandomSearchLoadig(false);
+    }
+  return (
+    <>
+    
+    <from className='flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl'
+    onsubmit={handleSubmit}
+      >
+    <CiSearch className='text-xl text-gray-500 mr-3' />
+        <input type="text" className='flex-grow focus:outline-none' 
+        onChange={(e)=>setInput(e.target.value)}
+        />
+    <FaMicrophone className='text-lg'/>
+    </from>
+    <div className='flex flex-col space-y-2 sm:space-y-0 justify-center sm:flex-row mt-8' >
+        <button className='bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow' 
+            onClick={handleSubmit}
+          >Google Search</button>
+        <button    disabled={randomSearchLoadig} className='bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow' onClick={randomSearch}>
+            {randomSearchLoadig?'Loading...' :'I am feeling Lucky'}
+        </button>
+    </div>
+    </>
+  )
+}
+
+export default HomeSeacrch
